c76a1397c3f37976a851216ea4bc2cec341d96ac
Aman261008 <79710138+Aman261008@users.noreply.github.com>
2022-05-08 20:24:52 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,6 @@
+number=5
+guess=int(input("Enter your guess between 1 to 9 :"))
+if(guess==number):
+    print("Congratulation You Won !!!")
+else:
+    print("You Lose !!! . The number is",number)    
